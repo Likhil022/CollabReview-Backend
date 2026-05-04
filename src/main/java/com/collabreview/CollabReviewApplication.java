c2d41cfec763d7320ee39f47dd71794230645ddd
Likhil022 <likhilkosuru123@gmail.com>
2026-05-04 23:12:02 +0530

    init

---
+++ src/main/java/com/collabreview/CollabReviewApplication.java
@@ -0,0 +1,13 @@
+package com.collabreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collabreview/CollabReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
